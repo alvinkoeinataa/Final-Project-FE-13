c88e6697c388446fcdcc7958cc984e4679f31932
Alvin <alvinkoeinata@gmail.com>
2023-09-04 10:45:45 +0700

    update terbaru

---
+++ src/pages/product/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const Product = () => {
+  return (
+    <button>
+      <Link href="/login">login</Link>
+    </button>
+  );
+};
+
+export default Product;
